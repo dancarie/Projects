de6c31f5f69d73269398d5ae980448b271323467
dancarie <dan.cariello@gmail.com>
2017-10-29 04:47:44 -0400

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -2,3 +2,11 @@ import os
 import pandas as pd
 import requests
 PATH = r'/Users/danka/Projects/datatrfrm'
+r =
+requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iri
+s/iris.data')
+with open(PATH + 'iris.data', 'w') as f:
+ f.write(r.text)
+os.chdir(PATH)
+df = pd.read_csv(PATH + 'iris.data', names=['sepal length', 'sepal width',
+'petal length', 'petal width', 'class'])
